bc50705b8a31a1701d678fae07f81d50d4d25437
Rishav Sharma <uicandy@live.com>
2020-11-05 05:37:07 +0100

    Update Readme: - Add proptypes

--- src/app/components/Geo.js
+++ src/app/components/Geo.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React, { useRef } from 'react';
 import { Box } from 'rebass';
 import PropTypes from 'prop-types';
@@ -7,17 +6,18 @@ import { renderToString } from 'react-dom/server';
 import mapStyle from 'app/assets/mapStyle';
 import Map from './Map';
 
+const Template = ({ eventName, placeDetails }) => {
+  return (
+    <div>
+      <h3>{eventName}</h3>
+      <p>{placeDetails.formatted_address}</p>
+    </div>
+  );
+};
+
 const Geo = ({ location, setPlace, placeId, title, place }) => {
   const ref = useRef();
 
-  const Template = ({ eventName, placeDetails }) => {
-    return (
-      <div>
-        <h3>{eventName}</h3>
-        <p>{placeDetails.formatted_address}</p>
-      </div>
-    );
-  };
   // Ideally we'd want to display these all on the map at once and then link back to the results in the view - based on volume it can get expensive
   // At the very least we should be able to start with the first result's pin on the map - I'm using a dummy event for demo.
   const links = [
@@ -86,6 +86,12 @@ const Geo = ({ location, setPlace, placeId, title, place }) => {
   );
 };
 
+Template.propTypes = {
+  placeDetails: PropTypes.shape({
+    formatted_address: PropTypes.string.isRequired,
+  }).isRequired,
+  eventName: PropTypes.string.isRequired,
+};
 Geo.propTypes = {
   location: PropTypes.shape({
     lat: PropTypes.number.isRequired,
@@ -98,7 +104,6 @@ Geo.propTypes = {
     formatted_address: PropTypes.string.isRequired,
   }).isRequired,
   title: PropTypes.string.isRequired,
-  eventName: PropTypes.string.isRequired,
   placeId: PropTypes.string.isRequired,
   setPlace: PropTypes.func.isRequired,
 };
